bc36228c0a113502d92e5c1c80382a8fb8a0e675
mundmina <n.m.f.amina@gmail.com>
2026-03-16 17:56:55 +0500

    Refactor ForecastModel with freezed

--- lib/models/forecast_model.dart
+++ lib/models/forecast_model.dart
@@ -1,19 +1,20 @@
-class ForecastModel {
-  final String date;
-  final double temperature;
-  final String icon;
+import 'package:freezed_annotation/freezed_annotation.dart';
 
-  ForecastModel({
-    required this.date,
-    required this.temperature,
-    required this.icon,
-  });
+part 'forecast_model.freezed.dart';
+part 'forecast_model.g.dart';
 
-  factory ForecastModel.fromJson(Map<String, dynamic> json) {
-    return ForecastModel(
-      date: json['dt_txt'],
-      temperature: (json['main']['temp'] as num).toDouble(),
-      icon: json['weather'][0]['icon'],
-    );
-  }
+@freezed
+abstract class ForecastModel with _$ForecastModel {
+  const factory ForecastModel({
+    @JsonKey(name: 'dt_txt') required String date,
+    @JsonKey(readValue: _readTemp) required double temperature,
+    @JsonKey(readValue: _readIcon) required String icon,
+  }) = _ForecastModel;
+
+  factory ForecastModel.fromJson(Map<String, dynamic> json) =>
+      _$ForecastModelFromJson(json);
 }
+
+double _readTemp(Map json, String key) =>
+    (json['main']['temp'] as num).toDouble();
+String _readIcon(Map json, String key) => json['weather'][0]['icon'];
